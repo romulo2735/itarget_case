3653cfd10d68db9f2ef52c346f436a5e3fda471c
Romulo Sousa <romulo.sousa@artit.com.br>
2024-07-07 21:31:43 -0300

    feat: adicionando a camada de autenticação

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\{EventController, RegistrationController};
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('/events', EventController::class);
+    Route::resource('/registrations', RegistrationController::class);
+});
