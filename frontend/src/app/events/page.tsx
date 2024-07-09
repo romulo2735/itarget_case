caddd536b74ae59b77584a2e25f37cec35d5e0b5
Romulo Sousa <romulo.sousa@artit.com.br>
2024-07-08 22:28:49 -0300

    feat: Listagem de evento, realizar a inscrição e listar os usuários inscritos

--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -1,6 +1,6 @@
-"use client";
+"use client"
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { IEvent } from '@/interfaces/IEvent';
 import apiService from '@/utils/axiosConfig';
@@ -15,14 +15,23 @@ import {
     Paper,
     Typography,
     Pagination,
-    Box
+    Box,
+    Button
 } from '@mui/material';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import Registration from "@/components/RegistrationForm";
+import RegistrationsList from "@/components/RegistrationsList";
 
 const Events = () => {
     const [events, setEvents] = useState<IEvent[]>([]);
     const [page, setPage] = useState(1);
     const [pageSize, setPageSize] = useState(10);
     const [total, setTotal] = useState(0);
+    const [selectedEvent, setSelectedEvent] = useState<IEvent | null>(null);
+    const [viewRegistrationsEvent, setViewRegistrationsEvent] = useState<IEvent | null>(null);
     const router = useRouter();
 
     useEffect(() => {
@@ -60,6 +69,19 @@ const Events = () => {
         setPage(1);
     };
 
+    const handleRegisterClick = (event: IEvent) => {
+        setSelectedEvent(event);
+    };
+
+    const handleViewRegistrationsClick = (event: IEvent) => {
+        setViewRegistrationsEvent(event);
+    };
+
+    const handleClose = () => {
+        setSelectedEvent(null);
+        setViewRegistrationsEvent(null);
+    };
+
     return (
         <Container>
             <Typography variant="h4" component="h1" gutterBottom>
@@ -76,10 +98,12 @@ const Events = () => {
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Start Date</TableCell>
-                            <TableCell>End Date</TableCell>
-                            <TableCell>Status</TableCell>
+                            <TableCell>Nome</TableCell>
+                            <TableCell>Inicio</TableCell>
+                            <TableCell>Fim</TableCell>
+                            <TableCell>Ativo</TableCell>
+                            <TableCell>Increver-se</TableCell>
+                            <TableCell>Inscritos</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -88,7 +112,31 @@ const Events = () => {
                                 <TableCell>{event.name}</TableCell>
                                 <TableCell>{event.start_date}</TableCell>
                                 <TableCell>{event.end_date}</TableCell>
-                                <TableCell>{event.status ? 'Active' : 'Inactive'}</TableCell>
+                                <TableCell>{event.status ?
+                                    <CheckBoxIcon color={'success'} /> :
+                                    <IndeterminateCheckBoxIcon color={'error'} />
+                                }
+                                </TableCell>
+                                <TableCell>
+                                    <Button
+                                        disabled={!event.status}
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={() => handleRegisterClick(event)}
+                                    >
+                                        <PersonAddAlt1Icon />
+                                    </Button>
+                                </TableCell>
+                                <TableCell>
+                                    <Button
+                                        disabled={!event.status}
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={() => handleViewRegistrationsClick(event)}
+                                    >
+                                        <ReorderIcon />
+                                    </Button>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
@@ -102,6 +150,21 @@ const Events = () => {
                     color="primary"
                 />
             </Box>
+            {selectedEvent && (
+                <Registration
+                    eventId={selectedEvent.id}
+                    eventName={selectedEvent.name}
+                    onClose={handleClose}
+                />
+            )}
+            {viewRegistrationsEvent && (
+                <RegistrationsList
+                    eventId={viewRegistrationsEvent.id}
+                    eventName={viewRegistrationsEvent.name}
+                    open={!!viewRegistrationsEvent}
+                    onClose={handleClose}
+                />
+            )}
         </Container>
     );
 };
