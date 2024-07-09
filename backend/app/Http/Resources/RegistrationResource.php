85b10ff6f9fd1d1bd49467c62ff05590fd2ac8e6
Romulo Sousa <romulo.sousa@artit.com.br>
2024-07-09 00:18:21 -0300

    Resource Class

---
+++ backend/app/Http/Resources/RegistrationResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RegistrationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'cpf' => $this->cpf,
+            'email' => $this->email,
+            'event' => new EventResource($this->event),
+        ];
+    }
+}
